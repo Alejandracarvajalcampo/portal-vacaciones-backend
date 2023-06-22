251c19f1fea513810409abcef34544a7d8a0915a
Alejandra Carvajal <alejandracarvajal316210@gmail.com>
2023-06-22 10:43:14 -0500

    the vacationRequest entity and its class are created for the domain

---
+++ src/main/java/gestionvacacionesbackend/gestionvacaciones/persistence/entities/VacationRequestEntity.java
@@ -0,0 +1,120 @@
+package gestionvacacionesbackend.gestionvacaciones.persistence.entities;
+
+
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Table(name = "solicitud_vacaciones")
+@Entity
+public class VacationRequestEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "tipo_solicitud")
+    private String requestType;
+    @Column(name = "fecha_inicio")
+    private LocalDate startDate;
+    @Column(name = "fecha_fin")
+    private LocalDate endDate;
+    @Column(name = "fecha_contratacion")
+    private LocalDate hireDate;
+    @Column(name = "dias")
+    private Integer days;
+    @Column(name = "fecha")
+    private LocalDate date;
+    @Column(name = "comentario")
+    private String comment;
+
+    @ManyToOne
+//    @JsonBackReference
+    @JoinColumn(name = "document_id")
+    private EmployeeEntity employee;
+
+    public VacationRequestEntity() {
+    }
+
+    public VacationRequestEntity(Long id, String requestType, LocalDate startDate, LocalDate endDate, LocalDate hireDate, Integer days, LocalDate date, String comment, EmployeeEntity employee) {
+        this.id = id;
+        this.requestType = requestType;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.hireDate = hireDate;
+        this.days = days;
+        this.date = date;
+        this.comment = comment;
+        this.employee = employee;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRequestType() {
+        return requestType;
+    }
+
+    public void setRequestType(String requestType) {
+        this.requestType = requestType;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Integer getDays() {
+        return days;
+    }
+
+    public void setDays(Integer days) {
+        this.days = days;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public EmployeeEntity getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(EmployeeEntity employee) {
+        this.employee = employee;
+    }
+}
